e1a43ba8fc0bd03f700d0740413e36277cafeeca
Mateus Caletti <mateus@caletti.com.br>
2018-05-16 11:39:52 -0300

    Colocando estilo no Header

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 
 // Create a component
 const App = () => {
-    return <Header />;
-}
+    return <Header headerText={'Cliente'} />
+};
 
 // Render it to the device
 AppRegistry.registerComponent('poc_reactnative', () => App);
