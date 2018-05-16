43c3c29079c1c451360e20cf82cda5db4a74c45f
Mateus Caletti <mateus@caletti.com.br>
2018-05-16 11:34:00 -0300

    Usando o componente Header

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
 
 // Create a component
 const App = () => {
-    return <Text>Some Text</Text>;
+    return <Header />;
 }
 
 // Render it to the device
